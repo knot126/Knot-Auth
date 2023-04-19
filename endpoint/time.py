d72bc11467d0ac03bde30cd0491d544eee9c4b2d
knot126 <73200930+knot126@users.noreply.github.com>
2023-04-19 06:09:20 +0000

    asdf

--- endpoint/time.py
+++
@@ -1,9 +0,0 @@
-import util
-
-def main(context):
-	context.respond({
-		"status": "done",
-		"message": "Current server time was gotten successfully.",
-		"time": util.time(),
-		"year_approx": 1970 + util.time() // (60 * 60 * 24 * 365.25),
-	})
